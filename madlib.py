9d14291a977669882788609a5491f9f0bdfc3e93
Kaitlyn Stauder <kaitlyn.stauder1@marist.edu>
2018-01-30 15:53:44 -0500

    Solution to Mad Lib exercise.

---
+++ madlib.py
@@ -0,0 +1,13 @@
+# Introduction to Programming
+# Author: Kaitlyn Stauder
+# Date: January 29, 2018
+
+def main():
+    print("Enter a word for each entry below.")
+    noun = input("Enter a noun:")
+    verb = input("Enter a verb:")
+    adjective = input("Enter an adjective:")
+    place = input("Enter a place:")
+    print("Take your", adjective, noun, "and", verb, "it at the", place)
+    
+main()
