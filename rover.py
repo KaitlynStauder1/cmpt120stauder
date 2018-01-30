567ef269bd23e336b188e4776804884d710fa726
Kaitlyn Stauder <kaitlyn.stauder1@marist.edu>
2018-01-30 15:37:30 -0500

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,11 @@
+# Introduciton to Programming
+# Author: Kaitlyn Stauder
+# Date: January 29, 2018
+
+def convert():
+    distance = 34000000
+    speed = 186000
+    time = distance/speed
+    print("It takes the Curiosity", time, "seconds to reach NASA when Mars is at its closest orbit to Earth.")
+    
+convert()
