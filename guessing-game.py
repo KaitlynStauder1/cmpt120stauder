f15983b335f57b702297f49500a79c30fd0a760c
Kaitlyn Stauder <kaitlyn.stauder1@marist.edu>
2018-02-26 18:48:32 -0500

    Accept the answer regardless of upper/lower case, Resolve #3.

--- guessing-game.py
+++ guessing-game.py
@@ -6,16 +6,18 @@
 def main():
     print("The program is thinking of an animal.")
     guess = input("Guess the name of the animal: ")
+    guess2 = guess.lower()
     animal = "elephant"
     stop = "quit"
     no = False
     while not no:
-        if guess == stop:
+        if guess2 == stop:
             print("You have quit the game.")
             no = True
-        elif guess != animal:
+        elif guess2 != animal:
             print("That guess is incorrect, try again.")
             guess = input("Guess the name of the animal: ")
+            guess2 = guess.lower()
         else:
             print("Congratulations! You have correctly guessed the animal!")
             no = True
