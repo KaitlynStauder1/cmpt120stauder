e9c3dfa06a7a1c6d92bbb5da43a1dff1755bd1c9
Kaitlyn Stauder <kaitlyn.stauder1@marist.edu>
2018-03-30 23:13:27 -0400

    Project 3

--- calculator.pyw/calc_functions.py
+++ calculator.pyw/calc_functions.py
@@ -8,20 +8,38 @@ import math
 
 win = GraphWin('Calc', 400, 580)
 displayTextElement = Text(Point(0, 50), "green")
-calcGrid = [
-    [7, 8, 9, '+', 'MC'],
-    [4, 5, 6, '-', 'M+'],
-    [1, 2, 3, '*', 'M-'],
-    ['', 0, '', '/', 'MR'],
-    ['', 'sqrt', '', '+/-', 'MS'],
-    ['%', '^2', '.', 'Del', '=']
-]
-buttons = [['', '', '', '', ''],
-           ['', '', '', '', ''],
-           ['', '', '', '', ''],
-           ['', '', '', '', ''],
-           ['', '', '', '', ''],
-           ['', '', '', '', '']]
+calcGrid = [[7, 8, 9, '+', 'MC'],
+            [4, 5, 6, '-', 'M+'],
+            [1, 2, 3, '*', 'M-'],
+            ['', 0, '(', ')', 'MR'],
+            ['Mode', 'sqrt', '/', '+/-', 'MS'],
+            ['%', '^2', '.', 'Del', '=']]
+
+sciGrid = [['x^y', 'sin', 'cos', 'tan', '10^x'],
+           ['log', 'sin^-1', 'cos^-1', 'tan^-1', 'ln'],
+           [7, 8, 9, '+', 'MC'],
+           [4, 5, 6, '-', 'M+'],
+           [1, 2, 3, '*', 'M-'],
+           ['', 0, '(', ')', 'MR'],
+           ['Mode', 'sqrt', '/', '+/-', 'MS'],
+           ['%', '^2', '.', 'Del', '=']]
+    
+standardButtons = [['', '', '', '', ''],
+                   ['', '', '', '', ''],
+                   ['', '', '', '', ''],
+                   ['', '', '', '', ''],
+                   ['', '', '', '', ''],
+                   ['', '', '', '', '']]
+
+sciButtons = [['', '', '', '', ''],
+              ['', '', '', '', ''],
+              ['', '', '', '', ''],
+              ['', '', '', '', ''],
+              ['', '', '', '', ''],
+              ['', '', '', '', ''],
+              ['', '', '', '', ''],
+              ['', '', '', '', '']]
+
 
 def calcButton(x, y, value):
     button = Rectangle(Point(x, y), Point(x + 80, y + 80)) 
@@ -40,26 +58,33 @@ def inside(clicked, button):
             return True
     return False
 
-def clickedButton(clicked):
-    for i in range(6):
-        for j in range(5):
+def clickedButton(clicked, buttons):
+    for i in range(len(buttons)):
+        for j in range(len(buttons[0])):
             if clicked.getX() > buttons[i][j].p1.getX() and clicked.getX() < buttons[i][j].p2.getX():
                 if clicked.getY() > buttons[i][j].p1.getY() and clicked.getY() < buttons[i][j].p2.getY():
                     return i, j
     return -1, -1
 
-def createCalculatorButtons():
+def createStandardCalculatorButtons(buttons):
     for i in range (6):
         for j in range(5):
             buttons[i][j] = calcButton(j * 80, i * 80 + 100, calcGrid[i][j])
 
+def createSciCalculatorButtons(buttons):
+    for i in range(8):
+        for j in range(5):
+            buttons[i][j] = calcButton(j * 80, i * 80 + 100, sciGrid[i][j])
+
 def get_number(op):
-    if op =='':
-        return op
-    if '.' in op:
-        return float(op)
-    else:
-        return int(op)
+    if type(op) == str:
+        if op =='':
+            return op
+        if '.' in op:
+            return float(op)
+        else:
+            return int(op)
+    return op
     
 def add(op1, op2):
     return op1 + op2
@@ -85,154 +110,219 @@ def sqrt(op):
 def percent(op):
     return op / 100
 
+def xRaisedy(op1, op2):
+    return op1 ** op2
+
+def sine(op):
+    return math.sin(op)
+
+def cosine(op):
+    return math.cos(op)
+
+def tangent(op):
+    return math.tan(op)
+
+def tenRaisedx(op):
+    return 10 ** op
+
+def log(op):
+    return math.log10(op)
+
+def inversesine(op):
+    return math.asin(op)
+    
+def inversecosine(op):
+    return math.acos(op)
+
+def inversetangent(op):
+    return math.atan(op)
+
+def naturallog(op):
+    return math.log(op)
+
+def evaluateInputString(inputString):
+    while '(' in inputString:
+        for i in range(len(inputString) - 1, -1, -1):
+            if inputString[i] == '(':
+                break
+        for j in range(i + 1, len(inputString)):
+            if inputString[j] == ')':
+                break
+        expression = inputString[i + 1:j]
+        if j < len(inputString):
+            inputString = inputString[:i] + evaluateExpression(expression) + inputString[j + 1:]
+        else:
+            inputString = inputString[:i] + evaluateExpression(expression)
+    if ',' in inputString:
+        return evaluateExpression(inputString)
+    return inputString
+
+def evaluateExpression(expression):
+    for i in range(len(expression)):
+        if expression[i] == ',':
+            break
+    for j in range(i + 1, len(expression)):
+        if expression[j] == ',':
+            break
+    expression = expression.replace(',', '')
+    if j != len(expression) - 1:
+        j = j - 2
+    if i == 0:
+        operator = expression[:j + 1]
+        num1 = get_number(expression[j + 1:])
+        return str(evaluateSingleOpExpression(num1, operator))
+    else:
+        num1 = get_number(expression[:i])
+    if j < len(expression) - 1:
+        operator = expression[i:j + 1]
+        num2 = get_number(expression[j + 1:])
+        return str(evaluateMultiOpExpression(num1, num2, operator))
+    else:
+        operator = expression[i:]
+        return str(evaluateSingleOpExpression(num1, operator))
+
+def evaluateSingleOpExpression(number, operator):
+    if operator == '+/-':
+        return changeSign(number)
+    elif operator == '^2':
+        return square(number)
+    elif operator == 'sqrt':
+        return sqrt(number)
+    elif operator == '%':
+        return percent(number)
+    elif operator == 'sin':
+        return sine(number)
+    elif operator == 'cos':
+        return cosine(number)
+    elif operator == 'tan':
+        return tangent(number)
+    elif operator == '10^x':
+        return tenRaisedx(number)
+    elif operator == 'log':
+        return log(number)
+    elif operator == 'sin^-1':
+        return inversesine(number)
+    elif operator == 'cos^-1':
+        return inversecosine(number)
+    elif operator == 'tan^-1':
+        return inversetangent(number)
+    elif operator == 'ln':
+        return naturallog(number)
+
+def evaluateMultiOpExpression(number1, number2, operator):
+    if operator == '+':
+        return add(number1, number2)
+    elif operator == '-':
+        return subtract(number1, number2)
+    elif operator == '*':
+        return multiply(number1, number2)
+    elif operator == '/':
+        return divide(number1, number2)
+    elif operator == 'x^y':
+        return xRaisedy(number1, number2)
+
 def main():
-    createCalculatorButtons()
+    global win, calcGrid, sciGrid, standardButtons, sciButtons
+    grid = calcGrid
+    buttons = standardButtons
+    mode = 'standard'
+    createStandardCalculatorButtons(buttons)
+    inputString = ''
     displayString = ''
     displayTextElement = Text(Point(0, 50), "")
     displayTextElement.draw(win)
-    op1 = ''
-    op1Entered = False
-    op2 = ''
-    op2Entered = False
-    opr = ''
-    oprEntered = False
-    operators = ['+', '-', '*', '/', '+/-', '^2', 'sqrt', '%']
+    operators = ['+', '-', '*', '/', '+/-', '^2', 'sqrt', '%', 'x^y', 'sin', 'cos', 'tan', '10^x', 'log', 'sin^-1', 'cos^-1', 'tan^-1', 'ln']
     memoryButtons = ['MC', 'M+', 'M-', 'MR', 'MS']
-    answer = ''
     memory = 0
+    previousAnswer = ''
+    clearWindow = False
     while True:
         clicked = win.getMouse()
-        row, col = clickedButton(clicked)
-
+        row, col = clickedButton(clicked, buttons)
+        if clearWindow:
+            inputString = ''
+            displayString = ''
+            displayTextElement.undraw()
+            displayTextElement = Text(Point(0, 50), displayString)
+            displayTextElement.draw(win)
+            clearWindow = False
         if row >= 0:
-            userInput = calcGrid[row][col]
-            if userInput == 'Del':
+            userInput = grid[row][col]
+            if userInput not in ['Del', '=', 'Mode'] and userInput not in memoryButtons:
+                if str(userInput) in operators:
+                    inputString = inputString + ',' + str(userInput) + ','
+                else:
+                    inputString = inputString + str(userInput)
+                displayString = (displayString + str(userInput)).rjust(150)
                 displayTextElement.undraw()
-                displayString = ''
-                displayTextElement = Text(Point(0, 50), "")
+                displayTextElement = Text(Point(0, 50), displayString)
                 displayTextElement.draw(win)
-                op1Entered = False
-                op1 = ''
-                op2Entered = False
-                op2 = ''
-                oprEntered = False
-                answer = ''
             else:
-                if userInput == '=':
-                    previousInput = '='
-                    op1 = get_number(op1)
-                    op2 = get_number(op2)
-                    if opr == '+':
-                        answer = add(op1, op2)
-                    elif opr == '-':
-                        answer = subtract(op1, op2)
-                    elif opr == '*':
-                        answer = multiply(op1, op2)
-                    elif opr == '/':
-                        answer = divide(op1, op2)
-                    elif opr == '+/-':
-                        if op1Entered:
-                            answer = changeSign(op1)
-                        else:
-                            answer = changeSign(op2)
-                    elif opr == '^2':
-                        if op1Entered:
-                            answer = square(op1)
-                        else:
-                            answer = square(op2)
-                    elif opr == 'sqrt':
-                        if op1Entered:
-                            answer = sqrt(op1)
-                        else:
-                            answer = sqrt(op2)
-                    elif opr == '%':
-                        if op1Entered:
-                            answer = percent(op1)
-                        else:
-                            answer = percent(op2)
-                    else:
-                        answer = ''
+                if userInput == 'Del':
+                    inputString = ''
+                    displayString = ''
+                    previousAnswer = ''
                     displayTextElement.undraw()
-                    displayString = str(answer).rjust(150)
                     displayTextElement = Text(Point(0, 50), displayString)
                     displayTextElement.draw(win)
-                    op1 = ''
-                    op1Entered = False
-                    op2 = ''
-                    op2Entered = False
-                    opr = ''
-                    oprEntered = False
+                elif userInput == '=':
+                    previousAnswer = evaluateInputString(inputString)
+                    displayString = previousAnswer.rjust(150)
+                    displayTextElement.undraw()
+                    displayTextElement = Text(Point(0, 50), displayString)
+                    displayTextElement.draw(win)
+                    clearWindow = True
                 elif userInput in memoryButtons:
-                    op1 = get_number(op1)
-                    op2 = get_number(op2)
                     if userInput == 'MC':
-                        memory = 0
+                        inputString = ''
+                        displayString = ''
                         displayTextElement.undraw()
+                        displayTextElement = Text(Point(0, 50), displayString)
+                        displayTextElement.draw(win)
+                        memory = 0
                     elif userInput == 'MR':
                         displayTextElement.undraw()
                         displayString = str(memory).rjust(150)
                         displayTextElement = Text(Point(0, 50), displayString)
                         displayTextElement.draw(win)
+                        inputString = str(memory)
                     else:
-                        if userInput == 'M+':
-                            if answer != '':
-                                memory = add(memory, answer)
-                            else:
-                                if op1Entered:
-                                    memory = add(memory, op1)
-                                elif op2Entered:
-                                    memory = add(memory, op2)
-                        elif userInput == 'M-':
-                            if answer != '':
-                                memory = subtract(memory, answer)
-                            else:
-                                if op1Entered:
-                                    memory = subtract(memory, op1)
-                                elif op2Entered:
-                                    memory = subtract(memory, op2)
-                        elif userInput == 'MS':
-                            if answer != '':
-                                memory = answer
-                            else:
-                                if op1Entered:
-                                    memory = op1
-                                elif op2Entered:
-                                    memory = op2
-                        if op1Entered or op2Entered:
-                            displayTextElement.undraw()
+                        if inputString == '':
+                            if previousAnswer != '':
+                                if userInput == 'M+':
+                                    memory = memory + get_number(previousAnswer)
+                                elif userInput == 'M-':
+                                    memory = memory + get_number(previousAnswer)
+                                elif userInput == 'MS':
+                                    memory = memory + get_number(previousAnswer)
+                        else:
+                            if userInput == 'M+':
+                                memory = memory + get_number(evaluateInputString(inputString))
+                            elif userInput == 'M-':
+                                memory = memory - get_number(evaluateInputString(inputString))
+                            elif userInput == 'MS':
+                                memory = get_number(evaluateInputString(inputString))
                             displayString = str(memory).rjust(150)
+                            displayTextElement.undraw()
                             displayTextElement = Text(Point(0, 50), displayString)
                             displayTextElement.draw(win)
-                    op1 = ''
-                    op1Entered = False
-                    op2 = ''
-                    op2Entered = False
-                    opr = ''
-                    oprEntered = False
-                else:
-                    if not op1Entered and not op2Entered and not oprEntered:
-                        displayTextElement.undraw()
-                        displayString = ''
-                        displayTextElement = Text(Point(0, 50), "")
-                        displayTextElement.draw(win)
-                    if userInput in operators:
-                        oprEntered = True
-                        opr = userInput
-                    else:
-                        if not oprEntered:
-                            op1Entered = True
-                            op1 = op1 + str(userInput)
-                        else:
-                            op2Entered = True
-                            op2 = op2 + str(userInput)
-                    buttons[row][col].setFill('lightblue')
-                    displayString = (displayString + str(userInput)).rjust(150)
-                    displayTextElement.undraw()
-                    displayTextElement = Text(Point(0, 50), displayString)
-                    displayTextElement.draw(win)
+                        inputString = str(memory)
+                elif userInput == 'Mode':
+                    clearWindow = True
+                    if mode == 'standard':
+                        grid = sciGrid
+                        buttons = sciButtons
+                        win.close()
+                        win = GraphWin('Calc', 400, 730)
+                        createSciCalculatorButtons(buttons)
+                        mode = 'scientific'
+                    elif mode == 'scientific':
+                        grid = calcGrid
+                        buttons = standardButtons
+                        win.close()
+                        win = GraphWin('Calc', 400, 580)
+                        createStandardCalculatorButtons(buttons)
+                        mode = 'standard'                       
 
-        for i in range(6):
-            for j in range(5):
-                if (i == row and j == col):
-                    buttons[i][j].setFill('blue')        
-main()
+if __name__ == '__main__':
+    main()
